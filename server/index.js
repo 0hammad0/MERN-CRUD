9752d4b485f690348c1481d00ffe384587c31049
1hammad1 <hammadazam101010@gmail.com>
2023-02-18 11:23:48 +0500

    server insert

--- server/index.js
+++ server/index.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const FoodModel = require("./models/Food");
 
 const app = express();
 
 app.use(express.json());
 
-mongoose.connect("", {
-  useNewUrlParser: true,
+mongoose.connect(
+  "mongodb+srv://hammad:471754@mern-crud.78v8i5l.mongodb.net/crud?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+  }
+);
+
+app.get("/", async (req, res) => {
+  const food = new FoodModel({
+    foodName: "Apple",
+    eatingDays: 3,
+  });
+
+  try {
+    await food.save();
+    res.send("Data inserted");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.listen(3001, () => {
